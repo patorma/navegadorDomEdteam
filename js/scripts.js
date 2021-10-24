81e5ff4e0fc2c4b04b661cd6e9770e0d5a39141d
patorma <pocontreras@ing.ucsc.cl>
2021-10-24 18:19:43 -0300

    seleccioar nodos

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+const title = document.getElementById("title"),
+      title2 = document.querySelector("#title"),
+      listItems = document.querySelectorAll("ul li"),
+      secondItems = document.querySelector("ul li:nth-child(2)"),
+      list = document.getElementById('list')
+title.style.background = "yellow";
+//console.log(document.querySelector('#title'));
+
+console.log(title);
+console.log(title2);
+console.log(listItems);
+console.log(secondItems);
+console.log(list.querySelector('li:last-child'));
+
+//queremos encontrar uno de los hijos del list
+
