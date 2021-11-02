a2ca16836a43ce1d930c11d65e2940e6f23cbaa3
patorma <pocontreras@ing.ucsc.cl>
2021-11-01 21:37:37 -0300

    ejemplo de getAtribute() y setAtribute()

---
+++ js/scripts3.js
@@ -0,0 +1,10 @@
+const title = document.querySelector('h1');
+// getAtribute(): obtiene valor
+//setAtribute(): defina valor
+//hay que validar 
+// if(title){
+//     title.style.background = 'yellow';
+// }
+// title.setAttribute('id','title');
+title.id = 'title';
+console.log(title);
