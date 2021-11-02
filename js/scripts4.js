238168451f8e761a102a202de3d96f0045325f4d
patorma <pocontreras@ing.ucsc.cl>
2021-11-01 22:59:52 -0300

    se vio contenido y escritura en DOM

---
+++ js/scripts4.js
@@ -0,0 +1,10 @@
+const title = document.querySelector('h1');
+
+// title.textContent = 'Hola máma, estoy en clase';
+
+title.innerHTML = `Hola máma,<em>estoy en clase</em>`;
+/* son de lectura y escritura 
+console.log(title.textContent);
+console.log(title.innerHTML);
+
+console.log(title.outerHTML);*/
